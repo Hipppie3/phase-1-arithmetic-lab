433f2478a623310e915246af60076434ad4d8dbc
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-09-18 21:28:18 -0600

    commit tree 80f9dbbc3bc6b098c96f9742d3397d5eb01f5216

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,20);
+let random = (Math.floor(Math.random() * 5) + 0)
 // Write your code here
